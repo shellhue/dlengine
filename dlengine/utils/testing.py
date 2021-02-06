908afe7ed42b09a3e540e52c78a52926d0829530
黄泽宇 <huangzeyu@ibeiliao.com>
2021-02-06 19:28:36 +0800

    移除evaluation嵌套

--- dlengine/utils/testing.py
+++ dlengine/utils/testing.py
@@ -13,17 +13,14 @@ def print_csv_format(results):
     so that they are easy to copypaste into a spreadsheet.
 
     Args:
-        results (OrderedDict[dict]): task_name -> {metric -> score}
+        results (dict): {metric -> score}
     """
-    assert isinstance(results, OrderedDict), results  # unordered results cannot be properly printed
+    assert isinstance(results, dict), results  # unordered results cannot be properly printed
     logger = logging.getLogger(__name__)
-    for task, res in results.items():
-        # Don't print "AP-category" metrics since they are usually not tracked.
-        # important_res = [(k, v) for k, v in res.items() if "-" not in k]
-        important_res = [(k, v) for k, v in res.items()]
-        logger.info("copypaste: Task: {}".format(task))
-        logger.info("copypaste: " + ",".join([k[0] for k in important_res]))
-        logger.info("copypaste: " + ",".join(["{0:.4f}".format(k[1]) for k in important_res]))
+    print(results)
+    important_res = [(k, v) for k, v in results.items()]
+    logger.info("copypaste: " + ",".join([k[0] for k in important_res]))
+    logger.info("copypaste: " + ",".join(["{0:.4f}".format(k[1]) for k in important_res]))
 
 
 def verify_results(cfg, results):
